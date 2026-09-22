6677263eb82c5fb2ca4c04c00348188e8b57d46a
AosenC <82334948+Aosen-C@users.noreply.github.com>
2026-09-22 14:50:16 -0700

    Add files via upload

---
+++ FormatDate.java
@@ -0,0 +1,22 @@
+public class FormatDate {
+	
+	public static void printAmerican(String day, String month, int date, int year) {
+		
+		System.out.printf("%s, %s %d, %d\n", day, month, date, year);
+		
+		}
+		
+	public static void printEuropean(String day, String month, int date, int year) {
+		
+		System.out.printf("%s %d %s %d", day, date, month, year);
+		
+		}
+	
+	public static void main(String[] args) {
+		
+		printAmerican("Monday", "July", 22, 2019);
+		printEuropean("Monday", "July", 22, 2019);
+		
+		}
+	
+	}
